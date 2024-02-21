ffb7e1c2bac3997b77642229283beb0b14118960
mhcchm <marcoshcastello@usp.br>
2024-02-21 11:01:17 -0300

    Create the grid using javascript
    Signed-off-by: mhcchm <marcoshcastello@usp.br>

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const gridSize = 960;
+const divNumber = 16;
+const elementSize = Math.round(gridSize / divNumber);
+const grid = document.querySelector(".grid");
+
+for(let i=0;i<divNumber;i++){
+  for(let j=0;j<divNumber;j++){
+    const newDiv = document.createElement('div');
+    newDiv.style.minHeight = elementSize + 'px';
+    newDiv.style.minWidth = elementSize + 'px';
+    grid.appendChild(newDiv);
+  }
+}
