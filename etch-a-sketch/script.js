d5f047e9d76f549eb0c1dc66bece1540b9961d17
mhcchm <marcoshcastello@usp.br>
2024-02-21 12:58:25 -0300

    Add button functionality
    Signed-off-by: mhcchm <marcoshcastello@usp.br>

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,23 +1,48 @@
 const gridSize = 960;
-const divNumber = 16;
-const elementSize = Math.round(gridSize / divNumber);
+let divNumber = 16;
+let elementSize = Math.round(gridSize / divNumber);
 const grid = document.querySelector(".grid");
 
-for(let i=0;i<divNumber;i++){
-  for(let j=0;j<divNumber;j++){
-    const newDiv = document.createElement('div');
-    newDiv.style.minHeight = elementSize + 'px';
-    newDiv.style.minWidth = elementSize + 'px';
+function createGrid(quantity, size){
+  for(let i=0;i<quantity;i++){
+    for(let j=0;j<quantity;j++){
+      const newDiv = document.createElement('div');
+      newDiv.style.minHeight = size + 'px';
+      newDiv.style.minWidth = size + 'px';
 
-    newDiv.addEventListener("mouseenter", function(e){
-      newDiv.style.backgroundColor = 'black';
-    });
+      newDiv.addEventListener("mouseenter", function(e){
+        newDiv.style.backgroundColor = 'black';
+      });
 
-    newDiv.addEventListener("mouseleave", function(e){
-      setTimeout(function(){newDiv.style.backgroundColor = 'white';}, 1000);
-    });
+      newDiv.addEventListener("mouseleave", function(e){
+        setTimeout(function(){newDiv.style.backgroundColor = 'white';}, 1000);
+      });
 
 
-    grid.appendChild(newDiv);
+      grid.appendChild(newDiv);
+    }
   }
 }
+
+createGrid(divNumber, elementSize);
+
+function removeGridElements(){
+  while(grid.firstChild){
+    grid.removeChild(grid.firstChild);
+  }
+}
+
+const botao = document.querySelector(".botao");
+
+botao.addEventListener('click', function(e){
+  let chosenValue = 0;
+  while(chosenValue <= 0 || chosenValue > 100){
+    chosenValue = parseInt(prompt("Choose the new number of squares"));
+  }
+
+  removeGridElements();
+
+  divNumber = chosenValue;
+  elementSize = Math.round(gridSize / divNumber);
+  createGrid(divNumber, elementSize);
+});
