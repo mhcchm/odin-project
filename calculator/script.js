4eea48dc4924a30bd855c2e26058fd57ef5cca00
mhcchm <marcoshcastello@usp.br>
2024-02-24 20:59:08 -0300

    Update the webpage with the extra credits
    Signed-off-by: mhcchm <marcoshcastello@usp.br>

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,7 @@
 const tela = document.querySelector(".tela");
 const numericos = document.querySelector(".numericos");
 const operacoes = document.querySelector(".operacoes");
+const dot = document.querySelector(".num.dot");
 
 let caracteres = {
   "0" : 0,
@@ -27,25 +28,87 @@ let caracteres = {
   },
 };
 
+let simbolos = {
+  "+" : true,
+  "-" : true,
+  "*" : true,
+  "/" : true,
+  "" : true,
+  "NaN" : true,
+};
+
 tela.textContent = '';
 
 numericos.addEventListener("click", function(e){
   if(tela.textContent.length < 36 && (e.target.attributes.class.value === "num" || e.target.attributes.class.value === "num zero")){
     tela.textContent = tela.textContent + e.target.textContent;
+  
+  }else if(tela.textContent.length < 36 && e.target.attributes.class.value === "num dot"){
+    dot.removeEventListener("click", dotFunction);
   }
 });
 
+function dotFunction(e){
+  tela.textContent = tela.textContent + e.target.textContent;
+}
+dot.addEventListener("click", dotFunction);
+
 operacoes.addEventListener("click", function(e){
   if(e.target.textContent == "="){
-    operacao();
-    console.log("gerando resultado da expressao");
+    tela.textContent = operacao();
+  }else if(e.target.textContent == "C"){
+    tela.textContent = '';
   }else if(tela.textContent.length < 36){
     tela.textContent = tela.textContent + " " + e.target.textContent + " ";
+  }
+  dot.addEventListener("click", dotFunction);
+});
+
+document.addEventListener("keydown", function(e){
+  if(e.key === "Backspace"){
+    tela.textContent = tela.textContent.slice(0, tela.textContent.length-1);
+  }else if(tela.textContent.length < 36 && (e.key in caracteres && !(e.key in simbolos))){
+    tela.textContent = tela.textContent + e.key;
   }
 });
 
 function operacao(){
   let separados = tela.textContent.split(" ");
-  separados.forEach((num) => console.log(num));
-  tela.textContent = '';
+  if(separados.length == 0){
+    return "";
+  }
+
+  // passamos da esquerda para a direita realizando a operação entre o numero da expressao e o 
+  // valor atual da expressao. É necessário fazer uma passada na expressão checando se ela é válida
+  // checa se a expressão é válida
+  if(simbolos[separados[0]] === true){
+    console.error("Started the expression with an operator.");
+    return ""; 
+  }else if(separados[0] == "ERROR"){
+    return "";
+  }
+  for(let i=1;i<separados.length;i++){
+    if(simbolos[separados[i]] === true && simbolos[separados[i+1]] === true){
+      console.error("Invalid operation.");
+      return ""; 
+    }
+    if(separados[i] == 0 && separados[i-1] === "/"){
+      console.error("Tried to divide by zero.");
+      return "";
+    }
+  }
+
+  // agora sabemos que a expressao não é inválida devido as posições dos operadores
+  let resultado = caracteres[separados[1]](parseFloat(separados[0]), parseFloat(separados[2]));
+  for(let i=3;i<separados.length-1;i+=2){
+    let dir = parseFloat(separados[i+1]);
+    if(dir === 0 && separados[i] == "/"){
+      console.error("Tried to divide by zero.");
+      return "";
+    }
+    resultado = caracteres[separados[i]](resultado, dir);
+  }
+
+  //tela.textContent = resultado;
+  return resultado;
 }
